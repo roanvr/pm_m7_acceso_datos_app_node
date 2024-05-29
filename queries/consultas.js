5e209bd80c8d445b46a9c639780cadad066cf77f
RodrigoValdivia <rodrigo.valdivia@usach.cl>
2024-05-28 20:18:48 -0400

    finalización de desafío con consultas y rutas para transferencias

--- queries/consultas.js
+++ queries/consultas.js
@@ -27,6 +27,60 @@ const getUser = async () => {
     }
 };
 
+const updateUser = async(nombre, balance, id) => {
+    try {
+        const consulta = {
+            text: 'UPDATE usuarios SET nombre = $1, balance = $2 WHERE id = $3 RETURNING *',
+            values: [nombre, balance, id]
+        }
+        const response = await pool.query(consulta);
+        console.log(response.rows);
+        return response.rows;
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+const deleteUser = async(id) => {
+    try {
+        const consulta = {
+            text: 'DELETE FROM usuarios WHERE id = $1 RETURNING *',
+            values: [id]
+        }
+        const response = await pool.query(consulta);
+        console.log(response.rows);
+        return response.rows;
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+const addTransfer = async(data) => {
+    try {
+        const consulta = {
+            text:'INSERT INTO transferencias (emisor, receptor, monto, fecha) VALUES ($1,$2,$3,$4) RETURNING *',
+            values: data
+        }
+        const response = await pool.query(consulta);
+        console.log(response.rows);
+        return response.rows;
+        } catch(error) {
+        console.log(error.message); 
+    }
+};
+
+const getTransfer = async() => {
+    try {
+        const consulta = {
+            text: 'SELECT * FROM transferencias'
+        }
+        const response = await pool.query(consulta);
+        console.log(response.rows);
+        return response.rows
+        } catch(error) {
+        console.log(error.message); 
+    }
+};
 
 
-export { addUser, getUser };
+export { addUser, getUser, updateUser, deleteUser, addTransfer, getTransfer };
--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ import path from 'path';
 const router = express.Router();
 const __dirname = import.meta.dirname;
 import pool from '../config/db.js';
-import { addUser, getUser } from '../queries/consultas.js';
+import { addUser, getUser, updateUser, deleteUser, addTransfer, getTransfer } from '../queries/consultas.js';
 
 router.get('/', (req,res) => res.sendFile(path.join(__dirname, '../views/index.html')));
 
@@ -26,5 +26,47 @@ router.get('/usuarios', async(req,res) => {
         res.status(500).send(error);
     }
 });
+
+router.put('/usuario', async(req,res)=> {
+    try {
+        const { id } = req.query;
+        const {name, balance} = req.body;
+        const resultado = await updateUser(name, balance, id);
+        res.status(201).send(resultado)
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.delete('/usuario', async(req,res) => {
+    try {
+        const { id } = req.query;
+        const resultado = await deleteUser(id);
+        res.status(201).send(resultado);
+    } catch (error) {
+        res.status(500).send(error);  
+    }
+});
+
+router.get('/transferencias', async(req,res) => {
+    try {
+        const resultado = await getTransfer();
+        res.status(201).json(resultado);
+    } catch (error) {
+        res.status(500).send(error);  
+    }
+});
+
+router.post('/transferencias', async(req,res) => {
+    try {
+        const {nombre, balance} = req.body;
+        const data = [nombre, balance];
+        const resultado = await addTransfer(data);//consulta
+        res.status(201).send(resultado.rows);
+    } catch (error) {
+        res.status(500).send(error);  
+    }
+});
+
 
 export default router;
